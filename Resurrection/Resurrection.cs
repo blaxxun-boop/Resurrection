3552803fa29666d63063ac1fe2b192b8ecb65111
blaxxun <blaxxun@blaxxun.blaxxun>
2026-09-22 02:34:45 +0200

    update guilds API

--- Resurrection/Resurrection.cs
+++ Resurrection/Resurrection.cs
@@ -20,7 +20,7 @@ namespace Resurrection;
 public class Resurrection : BaseUnityPlugin
 {
 	private const string ModName = "Resurrection";
-	private const string ModVersion = "1.0.14";
+	private const string ModVersion = "1.0.15";
 	private const string ModGUID = "org.bepinex.plugins.resurrection";
 
 	private static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
